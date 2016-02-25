3cf7f6aa970c297f427dd72e3004deb21214d85f
geekAshim <ashimzid@gmail.com>
2016-02-25 15:15:43 +0530

    added Keys.cs

---
+++ AuScGen_GIT/WebDriverWrapper/Keys.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebDriverWrapper
+{
+    public class Keys
+    {
+        public static string Add 
+        { 
+            get
+            {
+                return OpenQA.Selenium.Keys.Add;
+            }
+        }
+
+        public static string Enter
+        {
+            get
+            {
+                return OpenQA.Selenium.Keys.Enter;
+            }
+        }
+
+        public static string KeyDown
+        {
+            get
+            {
+                return OpenQA.Selenium.Keys.ArrowDown;
+            }
+        }
+    }
+}
